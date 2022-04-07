c3692374fb786064441d53f14d1f36dadf52edf0
savasturkoglu1 <savasturkoglu@live.com>
2022-04-07 10:29:09 +0300

    1h timeframe prod 0.08

--- agent.py
+++ agent.py
@@ -99,27 +99,25 @@ class Agent:
                 self.expected_pnl = target
                 opposit = self.q_table[state,0]
                 leverage = max(np.ceil(target/0.15),3)
-                # if opposit/target<0.7:
-                #     action = 2
-                if opposit*-1/target<2.1 and target>0.15 and opposit<0 :
+                
+                if  target>0.15 and opposit<0 :
                     action = 1
                 else: 
                     action = 2
         elif action == 0:
           
                 self.expected_pnl = target
-                opposit = self.q_table[state,1]*-1
+                opposit = self.q_table[state,1]
                 leverage = max(np.ceil(target/0.15),3)
-                # if opposit/target<0.7:
-                #         action = 2
-                if opposit*-1/target<2.33 and target>0.15 and opposit<0 :
+               
+                if  target>0.15 and opposit<0 :
                     action = 0
                 else: 
                     action = 2
 
-        if position is not None:
-            if instant_pnl<self.expected_pnl:
-                action = 2
+        # if position is not None:
+        #     if instant_pnl<self.expected_pnl:
+        #         action = 2
        
                 
         
