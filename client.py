2c9aabd993de500b0bbdcdb28bddaaa15f5c4129
Savaş Türkoğlu <savasturkoglu@live.com>
2022-09-22 10:43:12 +0300

    Update client.py

--- client.py
+++ client.py
@@ -5,8 +5,8 @@ from binance.client import Client
 class iClient:
 
     def __init__(self):
-        self.api_key = "JKjrsJHkJoCNckrlXBdTjFB01iTszkNXmcqzUOkBj3Z6XROjnINVShqtmicseG58"
-        self.api_secret = "YraZCTbi5O1c2RzSbp0La5fZCPNoJR2t0Zl6PD20hBQwfhYKKK7pyiAEBUgs8Ex8"
+        self.api_key = "--"
+        self.api_secret = "--"
         self.client = Client(self.api_key, self.api_secret, {"timeout": 60}) #"verify": False, 
      
         
